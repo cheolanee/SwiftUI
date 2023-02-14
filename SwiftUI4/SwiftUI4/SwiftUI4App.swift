e5016c08aa658d0eabf68e8c1d5ca8107956249b
Ber <ber@inven.co.kr>
2023-02-14 22:14:38 +0900

    init project

---
+++ SwiftUI4/SwiftUI4/SwiftUI4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI4App.swift
+//  SwiftUI4
+//
+//  Created by 배철환 on 2023/02/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
